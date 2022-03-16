f8aea17d494c69cadfad69598074787608228a66
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-16 12:13:52 +0100

    Added state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+//importing data
+import data from './data';
+
 //importing styles
 import './styles/app.scss';
+
 //importing components
 import Player from './components/Player';
 import Song from './components/Song';
 
 function App() {
+  //state
+const [songs, setSongs] = useState (data());
+const [currentSong, setCurrentSong] =useState(songs[0])
   return (
     <div className="App">
-      <Song/>
+      <Song currentSong ={currentSong}/>
       <Player/>
       
     </div>
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 
-const Song =() =>{
+const Song =({currentSong}) =>{
 return(
     <div className="song-container">
 
-        <h1>Picture</h1>
-        <h2>Song</h2>
-        <h3>Artist</h3>
+        <img src={currentSong.cover} alt='cover' />
+        <h2>{currentSong.name}</h2>
+        <h3>{currentSong.artist}</h3>
     </div>
 )
 }
