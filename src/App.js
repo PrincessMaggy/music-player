9f017a0c8d808c3fc96b59643b3ac96175aea938
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-16 12:37:31 +0100

    play and pause

--- src/App.js
+++ src/App.js
@@ -13,11 +13,16 @@ import Song from './components/Song';
 function App() {
   //state
 const [songs, setSongs] = useState (data());
-const [currentSong, setCurrentSong] =useState(songs[0])
+const [currentSong, setCurrentSong] =useState(songs[0]);
+const [isPlaying, setIsPlaying] =useState(false);
   return (
     <div className="App">
-      <Song currentSong ={currentSong}/>
-      <Player currentSong={currentSong}/>
+      <Song 
+        currentSong ={currentSong}/>
+      <Player 
+        isPlaying={isPlaying} 
+        setIsPlaying ={setIsPlaying} 
+        currentSong={currentSong}/>
       
     </div>
   );
