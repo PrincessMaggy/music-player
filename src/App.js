e6dd3ad8167de71ab9b5f1f053d40d65f6616ce8
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-16 10:47:33 +0100

    initial styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+//importing styles
+import './styles/app.scss';
+//importing components
 import Player from './components/Player';
 import Song from './components/Song';
 
