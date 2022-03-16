e4cd8e3de54633fdc2e28ede3b6e565fd1141edd
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-16 10:30:11 +0100

    created components

---
+++ src/components/Song.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Song =() =>{
+return(
+    <div className="song-container">
+
+        <h1>Picture</h1>
+        <h2>Song</h2>
+        <h3>Artist</h3>
+    </div>
+)
+}
+
+
+export default  Song;
