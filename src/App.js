65b66d31528f2fe5453025946415d1b210904cf5
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-17 15:59:03 +0100

    Added navigation

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import './styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
 import Library from './components/Library';
+import Nav from './components/Nav';
 
 function App() {
 //Ref
@@ -23,6 +24,8 @@ const [songInfo, setSongInfo] =useState({
   currentTime:0,
   duration:0,
 });
+const [libraryStatus, setLibraryStatus] = useState(false);
+
 const timeUpdateHandler =(e) =>{
   const current = e.target.currentTime;
   const duration = e.target.duration;
@@ -34,6 +37,11 @@ const timeUpdateHandler =(e) =>{
 }
   return (
     <div className="App">
+      <Nav
+        libraryStatus={libraryStatus}
+        setLibraryStatus ={setLibraryStatus}
+      />
+
       <Song 
         currentSong ={currentSong}/>
 
@@ -52,6 +60,7 @@ const timeUpdateHandler =(e) =>{
         setCurrentSong ={setCurrentSong}
         isPlaying={isPlaying}
         setSongs={setSongs}
+        libraryStatus={libraryStatus}
         />
 
       <audio 
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,21 +1,21 @@
 import React from "react";
 import LibrarySong from './LibrarySong';
 
-const Library =({songs,audioRef, setCurrentSong, isPlaying, setSongs}) =>{
+const Library =({songs,audioRef, setCurrentSong, isPlaying, setSongs, libraryStatus}) =>{
 return(
-    <div className="library">
+    <div className={`library ${libraryStatus ? 'active-library' : ""} `}>
         <h2>Library</h2>
         <div className="library-songs"> 
         {songs.map((song) => (
           <LibrarySong
-           songs={songs}
-           song={song}
-           key={song.id}
-           id={song.id}
-           setCurrentSong={setCurrentSong}
-           audioRef={audioRef}
-           isPlaying={isPlaying}
-           setSongs={setSongs}
+                songs={songs}
+                song={song}
+                key={song.id}
+                id={song.id}
+                setCurrentSong={setCurrentSong}
+                audioRef={audioRef}
+                isPlaying={isPlaying}
+                setSongs={setSongs}
            />
         ))}
         </div>
